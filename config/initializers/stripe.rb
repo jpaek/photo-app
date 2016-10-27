1a5f6bfc6d66fd6ad2c3a463604f01f18ecfd0f3
Rails Udemy Course <coolpaek@yahoo.com>
2016-10-27 21:35:18 +0000

    Typo - Stripe, not stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,4 +2,4 @@ Rails.configuration.stripe = {
   :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
   :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
-stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
